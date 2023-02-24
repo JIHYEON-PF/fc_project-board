f55d4410ee69632ff6a826d6ec6e5e73a6e3492c
JIHYEON <jhkim@aptner.com>
2023-02-24 15:46:56 +0900

    feature:#2 - QueryDSL지원을 활용한 API 검색옵션 추가
    - `QueryPredicateExcutor`, `QuerydslBinderCustomizer` 활용하여 설정

--- fc-project-board/src/main/java/com/fc/projectboard/repository/ArticleRepository.java
+++ fc-project-board/src/main/java/com/fc/projectboard/repository/ArticleRepository.java
@@ -1,9 +1,29 @@
 package com.fc.projectboard.repository;
 
 import com.fc.projectboard.domain.Article;
+import com.fc.projectboard.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>
+        , QuerydslPredicateExecutor<Article>
+        , QuerydslBinderCustomizer<QArticle> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        bindings.excludeUnlistedProperties(true); // 필터링 리스트 초기화
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy); //필터링 리스트 셋업
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase); // search param은 first(하나) / StringExpression으로 대소문자 구분 없이 조회(like 검색)
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
