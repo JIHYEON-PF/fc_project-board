5a8811611aa1f9a8d9731cdeb0a5c6d4cb9b0f1b
JIHYEON <jhkim@aptner.com>
2023-03-02 21:19:40 +0900

    feature:#22 - 서비스 로직 테스트 코드 업데이트 및 DTO 재설계, 쿼리메소드 작성
    - 테스트 코드의 작성과 연관된 DTO 작성
    - 테스트에서 필요로 하는 Repository 쿼리 메소드 작성

--- fc-project-board/src/main/java/com/fc/projectboard/service/ArticleService.java
+++ fc-project-board/src/main/java/com/fc/projectboard/service/ArticleService.java
@@ -2,10 +2,11 @@ package com.fc.projectboard.service;
 
 import com.fc.projectboard.domain.type.SearchType;
 import com.fc.projectboard.dto.ArticleDto;
-import com.fc.projectboard.dto.ArticleUpdateDto;
+import com.fc.projectboard.dto.ArticleWithCommentsDto;
 import com.fc.projectboard.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,12 +18,12 @@ public class ArticleService {
     private final ArticleRepository articleRepository;
 
     @Transactional(readOnly = true)
-    public Page<ArticleDto> searchArticles(SearchType title, String searchKeyword) {
+    public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
         return Page.empty();
     }
 
     @Transactional(readOnly = true)
-    public ArticleDto searchArticle(long l) {
+    public ArticleWithCommentsDto getArticle(Long articleId) {
         return null;
     }
 
@@ -30,7 +31,7 @@ public class ArticleService {
         // TODO document why this method is empty
     }
 
-    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+    public void updateArticle(ArticleDto dto) {
         // TODO document why this method is empty
     }
 
