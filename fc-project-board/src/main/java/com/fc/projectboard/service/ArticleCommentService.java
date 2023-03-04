e40aec0f5e996ca55b92db24bc0a19b5bfe10657
JIHYEON <jhkim@aptner.com>
2023-03-05 00:43:30 +0900

    feature:#22 - 댓글 기능 구현 및 전체 테스트 동작 확인
    - 댓글 기능 구현 및 테스트 확인

--- fc-project-board/src/main/java/com/fc/projectboard/service/ArticleCommentService.java
+++ fc-project-board/src/main/java/com/fc/projectboard/service/ArticleCommentService.java
@@ -1,14 +1,19 @@
 package com.fc.projectboard.service;
 
+import com.fc.projectboard.domain.ArticleComment;
 import com.fc.projectboard.dto.ArticleCommentDto;
 import com.fc.projectboard.repository.ArticleCommentRepository;
 import com.fc.projectboard.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Slf4j
 @RequiredArgsConstructor
 @Transactional
 @Service
@@ -19,15 +24,32 @@ public class ArticleCommentService {
 
     @Transactional(readOnly = true)
     public List<ArticleCommentDto> searchArticleComments(Long articleId) {
-        return List.of();
+        return articleCommentRepository.findByArticle_Id(articleId).stream()
+                .map(ArticleCommentDto::from)
+                .toList();
     }
 
     public void saveArticleComment(ArticleCommentDto dto) {
+        try {
+            articleCommentRepository.save(dto.toEntity(articleRepository.getReferenceById(dto.articleId())));
+        } catch (EntityNotFoundException e) {
+            log.warn("Save comment is failed, Cannot find article - dto : {}", dto);
+        }
     }
 
     public void updateArticleComment(ArticleCommentDto dto) {
+        try {
+            ArticleComment articleComment = articleCommentRepository.getReferenceById(dto.id());
+
+            if (dto.content() != null) {
+                articleComment.setContent(dto.content());
+            }
+        } catch (EntityNotFoundException e) {
+            log.warn("Update comment is failed, Cannot find comment - dto : {}", dto);
+        }
     }
 
     public void deleteArticleComment(Long articleCommentId) {
+        articleCommentRepository.deleteById(articleCommentId);
     }
 }
