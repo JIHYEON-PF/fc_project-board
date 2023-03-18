45965ffa04d83fc257beae991314fa6a5d74ca6e
JIHYEON <jhkim@aptner.com>
2023-03-18 21:13:32 +0900

    feature:#67 - 해시태그 도메인이 신규 추가
    - 해시태그 도메인 추가로 인한 변경점 리팩토링
    - 빌드는 가능하지만, 일부 테스트는 통과하지 못하는 상태
    - 새로 추가된 도메인의 경우 향후 서비스 로직 구현의 이슈 발행하여 테스트 통과 및 정상기능 하도록 Refactoring 예정

--- src/main/java/com/fc/projectboard/repository/querydsl/ArticleRepositoryCustom.java
+++ src/main/java/com/fc/projectboard/repository/querydsl/ArticleRepositoryCustom.java
@@ -1,8 +1,20 @@
 package com.fc.projectboard.repository.querydsl;
 
+import com.fc.projectboard.domain.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Collection;
 import java.util.List;
 
 public interface ArticleRepositoryCustom {
 
+    /**
+     * @Deprecated 해시태그 도메인을 새로 만들었으므로 게시글의 해시코드를 조회하는 Query는 불필요
+     * @see HashtagRepositoryCustom#findAllHashtagNames()
+     */
+    @Deprecated
     List<String> findAllDistinctHashtags();
+
+    Page<Article> findByHashtagNames(Collection<String> hashtagName, Pageable pageable);
 }
