ca309a02b544f780363af6d7cb000af117821db6
JIHYEON <jhkim@aptner.com>
2023-03-19 15:42:54 +0900

    feature:#75 - 1dept 대댓글 도메인의 설계
    - 댓글 도메인의 안에서 부모 댓글과 자식 댓글의 관계를 매핑할 수 있도록 처리
    - 자식 댓글의 컬렉션 변화가 쿼리에 자동적으로 반영될 수 있도록 cascadeType.ALL 적용
    - 단방형 연결을 위해 객체가 아닌 Long타입의 프로퍼티 선언
    - 도메인에서 자식댓글을 추가할 수 있도록 메서드 구현

--- src/main/java/com/fc/projectboard/domain/ArticleComment.java
+++ src/main/java/com/fc/projectboard/domain/ArticleComment.java
@@ -5,7 +5,9 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @ToString(callSuper = true)
@@ -21,21 +23,44 @@ public class ArticleComment extends AuditingFields {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Setter @ManyToOne(optional = false) @JoinColumn(name = "userId") private UserAccount userAccount;
+    @Setter
+    @ManyToOne(optional = false)
+    private Article article; // 게시글 id
 
-    @Setter @ManyToOne(optional = false) private Article article; // 게시글 id
-    @Setter @Column(nullable = false, length = 500) private String content; // 댓글 본문
+    @Setter
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "userId")
+    private UserAccount userAccount;
+
+    @Setter
+    @Column(updatable = false)
+    private Long parentCommentId;
+
+    @ToString.Exclude
+    @OrderBy("createdAt ASC ")
+    @OneToMany(mappedBy = "parentCommentId", cascade = CascadeType.ALL)
+    private Set<ArticleComment> childComments = new LinkedHashSet<>();
+
+    @Setter
+    @Column(nullable = false, length = 500)
+    private String content; // 댓글 본문
 
     protected ArticleComment() {}
 
-    private ArticleComment(Article article, UserAccount userAccount, String content) {
+    private ArticleComment(Article article, UserAccount userAccount, Long parentCommentId, String content) {
         this.article = article;
         this.userAccount = userAccount;
+        this.parentCommentId = parentCommentId;
         this.content = content;
     }
 
     public static ArticleComment of(Article article, UserAccount userAccount, String content) {
-        return new ArticleComment(article, userAccount, content);
+        return new ArticleComment(article, userAccount, null, content);
+    }
+
+    public void addChildComment(ArticleComment child) {
+        child.setParentCommentId(this.getId());
+        this.getChildComments().add(child);
     }
 
     @Override
