fb4befc526e79234b916ff6eed89a86a9c19c2d5
JIHYEON <jhkim@aptner.com>
2023-03-21 23:00:32 +0900

    feature:#75 대댓글의 비즈니스 로직 구현
    - 댓글의 부모 댓글과 자식 댓글 관계를 만들고 entity < - > dto간의 변환 로직 수정
    - 댓글 서비스에서 댓글과 대댓글을 저장하고 삭제하는 방법 수정
    - 댓글과 대댓글을 DB로 부터 변환할 때 서로 계층구조를 만들고 각각 정렬하는 과정을 Comparator를 사용하여 구현

--- src/test/java/com/fc/projectboard/controller/ArticleCommentControllerTest.java
+++ src/test/java/com/fc/projectboard/controller/ArticleCommentControllerTest.java
@@ -86,4 +86,24 @@ class ArticleCommentControllerTest {
                 .andExpect(redirectedUrl("/articles/" + articleId));
         then(articleCommentService).should().deleteArticleComment(articleCommentId, userId);
     }
+
+    @WithUserDetails(value = "fkaaTest", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @DisplayName("[view][POST] 대댓글 등록 - 정상호출")
+    @Test
+    void givenArticleCommentInfoWithParentCommitId_whenrequesting_thenSavesNewChildComment() throws Exception {
+        //given
+        long articleId = 1L;
+        ArticleCommentRequest request = ArticleCommentRequest.of(articleId, 1L, "test comment");
+
+        //when & then
+        mvc.perform(
+                post("/comments/new")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .content(formDataEncoder.encode(request))
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/articles/" + articleId));
+
+        then(articleCommentService).should().saveArticleComment(any(ArticleCommentDto.class));
+    }
 }
